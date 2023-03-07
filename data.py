86fc70f9dd4f4448ca9971f85860772ad0dc43db
Ali Taslimi <121749081+alitaslimi@users.noreply.github.com>
2023-03-08 01:58:46 +0330

    Add files via upload

--- data.py
+++ data.py
@@ -226,38 +226,40 @@ def get_data(query):
         return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/296269d7-65e5-422d-a5f5-b28b65d3bf43/data/latest')
     
     # Social
-    
-    elif query == 'Social Creation Daily':
+    elif query == 'Social Overview':
         return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/7dfa5093-2701-4134-aba2-2645e1885e52/data/latest')
     
-    elif query == 'Social Changes Overview':
+    elif query == 'Social Daily':
+        return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/1f66be65-d7f8-4df9-bf2c-d7e9cffd843e/data/latest')
+    
+    elif query == 'Profile Changes Overview':
         return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/2e507b1d-5e0a-40f2-8f1a-96a5962795ae/data/latest')
     
-    elif query == 'Social Changes Daily':
+    elif query == 'Profile Changes Daily':
         return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/a66106f1-6349-48f1-8afe-a9bcc0146ff3/data/latest')
     
-    elif query == 'Social Tags Overview':
+    elif query == 'Profile Tags Overview':
         return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/a19cd13f-5d61-4750-8b7f-efc3024d59d5/data/latest')
     
-    elif query == 'Social Tags Daily':
+    elif query == 'Profile Tags Daily':
         return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/52070e5e-cbc4-45e4-95ce-35be6bd9b856/data/latest')
     
-    elif query == 'Social Linktree Overview':
+    elif query == 'Profile Linktree Overview':
         return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/f0bc3334-7c2a-426e-a243-2665adb9b57e/data/latest')
     
-    elif query == 'Social Linktree Daily':
+    elif query == 'Profile Linktree Daily':
         return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/37e88d51-9121-443d-a482-b6f3d40e2e2d/data/latest')
     
-    elif query == 'Social Image Overview':
+    elif query == 'Profile Image Overview':
         return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/b8f691a2-bebf-4711-a98f-43c01ca0c4ae/data/latest')
     
-    elif query == 'Social Image Daily':
+    elif query == 'Profile Image Daily':
         return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/e81bfbad-f31d-4b8a-b002-843d3cf0b5c9/data/latest')
     
-    elif query == 'Social NFTs Overview':
+    elif query == 'Profile NFTs Overview':
         return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/d0c5ef3b-f173-4b35-b81e-ac109e96b66d/data/latest')
     
-    elif query == 'Social NFTs Daily':
+    elif query == 'Profile NFTs Daily':
         return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/9ae52cb5-7abd-4497-bdf4-f7c1f2b5a3d0/data/latest')
     
     elif query == 'Social Actions Overview':
@@ -265,5 +267,29 @@ def get_data(query):
     
     elif query == 'Social Actions Daily':
         return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/fd67cbff-6180-42de-b0f5-575f77668aa1/data/latest')
+    
+    elif query == 'Social Activity Overview':
+        return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/3fd04ee6-1d12-41e8-9fe9-98213907078c/data/latest')
+    
+    elif query == 'Social Activity Daily':
+        return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/9a727f42-ebdf-4998-8890-9429d6b9469e/data/latest')
+    
+    elif query == 'Social Active Users':
+        return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/0221868e-b0d9-4f0f-99c8-3a79aa2692d7/data/latest')
+    
+    elif query == 'Social Following Daily':
+        return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/0824497f-a8c0-4869-a922-c786196398a7/data/latest')
+    
+    elif query == 'Social Following Counts':
+        return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/6582612a-2527-4129-9770-62690e188bc5/data/latest')
+    
+    elif query == 'Social Widgets Overview':
+        return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/b51c9aee-7c4f-43d9-b015-564f28b25545/data/latest')
+    
+    elif query == 'Social Widgets Daily':
+        return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/204b47f7-a3dc-4b22-88d9-8742360281d9/data/latest')
+    
+    elif query == 'Social Widgets Creators':
+        return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/04675497-866c-406e-b522-0ada2c2a44f7/data/latest')
     
     return None
